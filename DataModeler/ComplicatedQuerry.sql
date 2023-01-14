4f99d4f2f2a2d24b47a048b4530e475baa27ae7b
JovanSrdanov <93533441+JovanSrdanov@users.noreply.github.com>
2023-01-14 20:54:58 +0100

    Update ComplicatedQuerry.sql

--- DataModeler/ComplicatedQuerry.sql
+++ DataModeler/ComplicatedQuerry.sql
@@ -1,6 +1,6 @@
 -- Broj studenata po univerzitetu
 create or replace view 
-ComplicatedQuerry ( NazFak, BrojStud) As 
+ComplicatedQuerry ( NazUni, BrojStud) As 
 select u.nazuni, count(s.idosb)
 from fakultet f, univerzitet u, student s
 where f.univerzitet_iduni = u.iduni and s.fakultet_idfak= f.idfak
